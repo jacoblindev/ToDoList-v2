88e916d06911307babb19561d66db8433f721cb3
Jacob Lin <jacoblindev@gmail.com>
2020-03-01 22:20:11 +0800

    Finished the skeleton of the app!
    Set up the dependencies, database, routing and basic html.

---
+++ app.js
@@ -0,0 +1,53 @@
+// Dependencies
+const express = require("express"),
+      mongoose = require("mongoose"),
+      bodyParser = require("body-parser");
+
+const app = express();
+const port = 3000;
+const Schema = mongoose.Schema;
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+// DB connection
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
+
+// Set Schema
+const taskSchema = new Schema({
+  item: String
+});
+const listSchema = new Schema({
+  name: String,
+  tasks: [taskSchema]
+});
+
+// Set Model
+const List = mongoose.model("list", listSchema),
+      Task = mongoose.model("task", taskSchema);
+
+// Default sample lists
+const list0 = new List({name: "Daily Workout", tasks: [{item: "Run 3k"}, {item: "Code for 1h"}, {item: "50 pushup"}]}),
+      list1 = new List({name: "Work", tasks: [{item: "Reply Emails"}, {item: "Visit clients"}, {item: "Team meeting"}]}),
+      list2 = new List({name: "Shopping", tasks: [{item: "Cat Food"}, {item: "Mask"}, {item: "Sanitizer"}]}),
+      defaultLists = [list0, list1, list2];
+
+// Routing
+app.get("/", (req, res) => {
+  List.find({}, (err, lists) => {
+    if (lists.length === 0) {
+      // console.log("Nothing in the DB!!!");
+      List.insertMany(defaultLists, err => err ? console.log(err) : console.log("Insert Successfully!"));
+      res.redirect("/");
+    } else {
+      // res.send(`${lists[0].name} (${lists[0]._id}) : ${lists[0].tasks}`);
+      res.render("lists", {
+        listsName: lists
+      });
+    }
+  })
+});
+
+// Listen for request
+app.listen(port, () => console.log(`Server started on port: ${port}`));

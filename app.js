1a4b42db215a2948c213bbd15b0773571d0cfb07
Jacob Lin <jacoblindev@gmail.com>
2020-03-02 22:35:23 +0800

    Update front-end views
    Add express-ejs-layouts, adjusting the views and design the list page with css.

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 // Dependencies
 const express = require("express"),
       mongoose = require("mongoose"),
-      bodyParser = require("body-parser");
+      bodyParser = require("body-parser"),
+      expressLayouts = require("express-ejs-layouts");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const Schema = mongoose.Schema;
 
-app.set("view engine", "ejs");
+app.use(expressLayouts);
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set('layout', 'layouts/layout');
 
 // DB connection
 mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
